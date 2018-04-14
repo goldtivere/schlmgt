a3ec221eeb470a1a97bcb818bb41f3a1f6480aba
Gold <Gold@Gold>
2018-04-14 23:55:47 +0100

    schlmgt

---
+++ src/java/com/schlmgt/report/StudentReport.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.schlmgt.report;
+
+/**
+ *
+ * @author Gold
+ */
+public class StudentReport {
+
+    public void generateReport() {
+try
+{
+    //String report=
+    
+}
+catch(Exception ex)
+{
+    
+}
+    }
+
+}
