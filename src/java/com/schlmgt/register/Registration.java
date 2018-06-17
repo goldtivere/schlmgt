a59bd87140d88fc4c592e26c8c95861f44b5783f
Gold <Gold@Gold>
2018-06-17 22:57:39 +0100

    schlmgt

--- src/java/com/schlmgt/register/Registration.java
+++ src/java/com/schlmgt/register/Registration.java
@@ -5,11 +5,27 @@
  */
 package com.schlmgt.register;
 
+import com.schlmgt.dbconn.DbConnectionX;
+import com.schlmgt.imgupload.UploadImagesX;
+import com.schlmgt.login.UserDetails;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
@@ -22,10 +38,227 @@ public class Registration implements Serializable {
     private String registration;
     private String regType;
     private List<String> typeValue = new ArrayList<>();
+    private boolean studentStatus;
+    private boolean staffStatus;
+    private boolean but;
+    private boolean put;
+    private UploadedFile csv;
+    private String messangerOfTruth;
+
+    @PostConstruct
+    public void init() {
+        studentStatus = false;
+        staffStatus = false;
+        but = false;
+
+    }
+
+    public void regTypeChanges() {
+        if ("1".equalsIgnoreCase(getRegistration()) && "Data upload".equalsIgnoreCase(getRegType())) {
+            setStaffStatus(true);
+            setBut(false);
+            setPut(false);
+        } else if ("2".equalsIgnoreCase(getRegistration()) && "Data upload".equalsIgnoreCase(getRegType())) {
+            setStaffStatus(true);
+            setBut(false);
+            setPut(false);
+        } else if ("1".equalsIgnoreCase(getRegistration()) && "Data Entry".equalsIgnoreCase(getRegType())) {
+            setStaffStatus(false);
+            setBut(true);
+            setPut(false);
+        } else if ("2".equalsIgnoreCase(getRegistration()) && "Data entry".equalsIgnoreCase(getRegType())) {
+            setStaffStatus(false);
+            setBut(false);
+            setPut(true);
+        }
+    }
+
+    public void staffUpload(FileUploadEvent event) throws SQLException {
+        UploadImagesX uploadImagesX = new UploadImagesX();
+        DbConnectionX dbConnections = new DbConnectionX();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        con = dbConnections.mySqlDBconnection();
+        try {
+
+            InputStream mn = event.getFile().getInputstream();
+            XSSFWorkbook wb = new XSSFWorkbook(mn);
+            XSSFSheet ws = wb.getSheetAt(0);
+            Row row;
+        } catch (Exception exx) {
+
+        } finally {
+
+            if (!(con == null)) {
+                con.close();
+                con = null;
+            }
+            if (!(pstmt == null)) {
+                pstmt.close();
+                pstmt = null;
+            }
+
+        }
+    }
+
+    public void studentUpload(FileUploadEvent event) throws SQLException {
+        FacesMessage message;
+        FacesContext context = FacesContext.getCurrentInstance();
+        UploadImagesX uploadImagesX = new UploadImagesX();
+        DbConnectionX dbConnections = new DbConnectionX();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        con = dbConnections.mySqlDBconnection();
+        try {
+            List<String> studentDetails = new ArrayList<>();
+            studentDetails.add("SFirstName");
+            studentDetails.add("SMiddleName");
+            studentDetails.add("SLastName");
+            studentDetails.add("SDateOfBirth");
+            studentDetails.add("SPhoneNumber");
+            studentDetails.add("SEmailAddress");
+            studentDetails.add("Sex");
+            studentDetails.add("PFirstName");
+            studentDetails.add("PMiddleName");
+            studentDetails.add("PLastName");
+            studentDetails.add("Relationship");
+            studentDetails.add("PhoneNumber");
+            studentDetails.add("EmailAddress");
+            studentDetails.add("Country");
+            studentDetails.add("State");
+            studentDetails.add("LGA");
+            studentDetails.add("Address");
+            studentDetails.add("PreviousEdu");
+            studentDetails.add("PrevClass");
+            studentDetails.add("PrevGrade");
+            studentDetails.add("CurrentClass");
+            studentDetails.add("CurrentGrade");
+            studentDetails.add("Arm");
+            studentDetails.add("Term");
+            studentDetails.add("Year");
+            studentDetails.add("Disability");
+            studentDetails.add("BloodGroup");
+
+            InputStream mn = event.getFile().getInputstream();
+            XSSFWorkbook wb = new XSSFWorkbook(mn);
+            XSSFSheet ws = wb.getSheetAt(0);
+            Row row;
+            row = (Row) ws.getRow(0);
+            System.out.println(studentDetails.size() + " This is it " + row.getLastCellNum() + " damn " + row.getPhysicalNumberOfCells());
+            if (studentDetails.size() == row.getPhysicalNumberOfCells()) {
+                for (int i = 0; i < studentDetails.size(); i++) {
+                    if (row.getCell(i).toString().equalsIgnoreCase(studentDetails.get(i))) {
+                        System.out.println("not in right format" + row.getCell(i) + " and " + studentDetails.get(i) + " count " + i);
+                    } else {
+                        setMessangerOfTruth("Excel is in wrong format. It should be in this format: " + studentDetails.toString());
+                        message = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
+                        context.addMessage(null, message);
+                        break;
+                    }
+                }
+            } else {
+                setMessangerOfTruth("Excel is in wrong format. It should be in this format: " + studentDetails.toString());
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
+                context.addMessage(null, message);
+            }
+
+        } catch (Exception exx) {
+            exx.printStackTrace();
+        } finally {
+
+            if (!(con == null)) {
+                con.close();
+                con = null;
+            }
+            if (!(pstmt == null)) {
+                pstmt.close();
+                pstmt = null;
+            }
+
+        }
+    }
+
+    public void handleFileUpload(FileUploadEvent event) throws SQLException {
+
+        FacesMessage message;
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        try {
+            UserDetails userObj = (UserDetails) context.getExternalContext().getSessionMap().get("sessn_nums");
+            String on = String.valueOf(userObj);
+            String createdby = String.valueOf(userObj.getFirst_name() + " " + userObj.getLast_name());
+            int createdId = userObj.getId();
+
+            if ("1".equalsIgnoreCase(getRegistration())) {
+                studentUpload(event);
+                System.out.println(getRegistration() + "    " + getRegType() + "  Student");
+                setCsv(null);
+            } else if ("2".equalsIgnoreCase(getRegistration())) {
+                System.out.println(getRegistration() + "    " + getRegType() + "  Staff");
+                setCsv(null);
+            }
+        } catch (Exception ex) {
+            setMessangerOfTruth(ex.getMessage());
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
+            context.addMessage(null, message);
+
+        }
+    }
 
     public void regDetails() {
+        typeValue.clear();
         typeValue.add("Data Upload");
         typeValue.add("Data Entry");
+    }
+
+    public UploadedFile getCsv() {
+        return csv;
+    }
+
+    public String getMessangerOfTruth() {
+        return messangerOfTruth;
+    }
+
+    public void setMessangerOfTruth(String messangerOfTruth) {
+        this.messangerOfTruth = messangerOfTruth;
+    }
+
+    public void setCsv(UploadedFile csv) {
+        this.csv = csv;
+    }
+
+    public boolean isPut() {
+        return put;
+    }
+
+    public void setPut(boolean put) {
+        this.put = put;
+    }
+
+    public boolean isBut() {
+        return but;
+    }
+
+    public void setBut(boolean but) {
+        this.but = but;
+    }
+
+    public boolean isStudentStatus() {
+        return studentStatus;
+    }
+
+    public void setStudentStatus(boolean studentStatus) {
+        this.studentStatus = studentStatus;
+    }
+
+    public boolean isStaffStatus() {
+        return staffStatus;
+    }
+
+    public void setStaffStatus(boolean staffStatus) {
+        this.staffStatus = staffStatus;
     }
 
     public List<String> getTypeValue() {
---
+++ src/java/com/schlmgt/register/StudentModel.java
@@ -0,0 +1,261 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.schlmgt.register;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Gold
+ */
+public class StudentModel implements Serializable{
+    private String fname;
+    private String mname;
+    private String lname;
+    private Date dob;
+    private String pnum;
+    private String email;
+    private String sex;
+    private String pfname;
+    private String pmname;
+    private String plname;
+    private String relationship;
+    private String ppnum;
+    private String pemail;
+    private String country;
+    private String state;
+    private String lga;
+    private String address;
+    private String previousEdu;
+    private String previousClass;
+    private String previousGrade;
+    private String currentClass;
+    private String currentGrade;
+    private String arm;
+    private String term;
+    private String year;
+    private String Disability;
+    private String Bgroup;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getPnum() {
+        return pnum;
+    }
+
+    public void setPnum(String pnum) {
+        this.pnum = pnum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPfname() {
+        return pfname;
+    }
+
+    public void setPfname(String pfname) {
+        this.pfname = pfname;
+    }
+
+    public String getPmname() {
+        return pmname;
+    }
+
+    public void setPmname(String pmname) {
+        this.pmname = pmname;
+    }
+
+    public String getPlname() {
+        return plname;
+    }
+
+    public void setPlname(String plname) {
+        this.plname = plname;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(String relationship) {
+        this.relationship = relationship;
+    }
+
+    public String getPpnum() {
+        return ppnum;
+    }
+
+    public void setPpnum(String ppnum) {
+        this.ppnum = ppnum;
+    }
+
+    public String getPemail() {
+        return pemail;
+    }
+
+    public void setPemail(String pemail) {
+        this.pemail = pemail;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getLga() {
+        return lga;
+    }
+
+    public void setLga(String lga) {
+        this.lga = lga;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPreviousEdu() {
+        return previousEdu;
+    }
+
+    public void setPreviousEdu(String previousEdu) {
+        this.previousEdu = previousEdu;
+    }
+
+    public String getPreviousClass() {
+        return previousClass;
+    }
+
+    public void setPreviousClass(String previousClass) {
+        this.previousClass = previousClass;
+    }
+
+    public String getPreviousGrade() {
+        return previousGrade;
+    }
+
+    public void setPreviousGrade(String previousGrade) {
+        this.previousGrade = previousGrade;
+    }
+
+    public String getCurrentClass() {
+        return currentClass;
+    }
+
+    public void setCurrentClass(String currentClass) {
+        this.currentClass = currentClass;
+    }
+
+    public String getCurrentGrade() {
+        return currentGrade;
+    }
+
+    public void setCurrentGrade(String currentGrade) {
+        this.currentGrade = currentGrade;
+    }
+
+    public String getArm() {
+        return arm;
+    }
+
+    public void setArm(String arm) {
+        this.arm = arm;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDisability() {
+        return Disability;
+    }
+
+    public void setDisability(String Disability) {
+        this.Disability = Disability;
+    }
+
+    public String getBgroup() {
+        return Bgroup;
+    }
+
+    public void setBgroup(String Bgroup) {
+        this.Bgroup = Bgroup;
+    }
+    
+}
+
